360c3010f9502e9a92a27cd83baefc7f40330882
姚正发 <976691141@qq.com>
2018-03-19 16:04:32 +0800

    demo

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
-import * as http from 'http';
+import * as express from "express";
+import * as http from "http";
+import * as socketio from "socket.io";
 
-const server = http.createServer((request, response) => {
-    console.log("create a server..");
-    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.write("Hello world,we use typescript to develop.");
-    response.end();
+const app = express();
+const listener = http.createServer(app);
+const io = socketio(listener);
+
+app.get("/", (req, res) => {
+    res.sendfile(__dirname + "/static/index.html");
 });
 
-server.listen(3000, () => {
+io.on("connection",(socket) => {
+    console.log("a user connected");
+    socket.on('chat message', (msg: string) => {
+        console.log('message: ' + msg);
+    });
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+listener.listen(3000, () => {
     console.log("Server listening on port 3000");
     console.log("test...");
 });
