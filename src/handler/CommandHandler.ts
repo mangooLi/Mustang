a547f8f9f704d2eafea460da6faece610f87d1d3
姚正发 <976691141@qq.com>
2018-03-29 09:44:38 +0800

    增加IOC注入

--- src/handler/CommandHandler.ts
+++ src/handler/CommandHandler.ts
@@ -1,8 +1,15 @@
 import {Handler} from "./Handler";
 import {WSContext} from "../common/WSContext";
+import {injectable} from "inversify";
 import { NotImplementedError } from "../error/NotImplementedError";
 
+@injectable()
 export class CommandHandler implements Handler {
+
+    constructor() {
+        
+    }
+
     process(context: WSContext): boolean {
         throw new NotImplementedError();
     }
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -2,8 +2,10 @@ import {Container} from "inversify";
 import {TYPES} from "./types";
 import {ConfigurationManager} from "./configuration/ConfigurationManager";
 import {JsonConfigurationManager} from "./configuration/JsonConfigurationManager";
+import { CommandHandler } from "./handler/CommandHandler";
 
 const selfContainer = new Container();
 selfContainer.bind<ConfigurationManager>(TYPES.Configuration).to(JsonConfigurationManager).inSingletonScope();
+selfContainer.bind<CommandHandler>(TYPES.CommandHandler).to(CommandHandler);
 
 export { selfContainer };
